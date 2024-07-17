e6a0dffc3527844498cd27b2325dbf6590b6604f
Corey Farwell <coreyf@rwell.org>
2024-07-17 08:25:52 -0700

    it actually works

--- src/stories/Charts.stories.ts
+++ src/stories/Charts.stories.ts
@@ -18,12 +18,9 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    // primary: true,
-    // label: 'Button',
-    placeId: 0,
+    placeId: 674,
     filter: undefined,
   },
 };
