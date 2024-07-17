b2c537d580c8ee3b2d9399f56d5f24ff77a0b012
Corey Farwell <coreyf@rwell.org>
2024-07-17 08:45:25 -0700

    cleanup

--- src/stories/ChartTaxaSection.stories.ts
+++ src/stories/ChartTaxaSection.stories.ts
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { ChartTaxaSection } from "../components/charts";
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: "ChartTaxaSection",
   component: ChartTaxaSection,
--- src/stories/Charts.stories.ts
+++ src/stories/Charts.stories.ts
@@ -1,18 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Charts } from '../components/charts';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'Charts',
   component: Charts,
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  //   argTypes: {
-  //     backgroundColor: { control: 'color' },
-  //   },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  // args: { onClick: fn() },
 } satisfies Meta<typeof Charts>;
 
 export default meta;
